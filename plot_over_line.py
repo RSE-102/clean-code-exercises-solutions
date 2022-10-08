8377102a4ef39d7c5e8164331b61864f93693828
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-10-08 12:04:19 +0200

    simplify plot_over_line exercise

--- plot_over_line.py
+++ plot_over_line.py
@@ -1,90 +1,147 @@
-from typing import Tuple, List, Callable
-from math import nan, sqrt, sin, cos, pi
+# In this "solution", we reduced the number of arguments to
+# `plot_over_line` by combining the point cloud and the associated
+# discrete values into a `DiscreteFunction`, which allows for evaluation
+# at a given point via the __call__ operator. This has the benefit that
+# now the decision on how to evaluate the discrete function is now in here
+# and `plot_over_line` does not have to know about this.
+#
+# We also combined the start/end points of the line into a separate class `Line`,
+# which furthermore allows to get the points along a line for a given local coordinate
+# along the line between 0 and 1.
+#
+# Finally, we split the plot data generation from the actual plotting to make
+# it reusable in different contexts.
+
+from __future__ import annotations
+from typing import List, Tuple
+from dataclasses import dataclass
+from math import pi, sqrt, sin, cos
 from matplotlib.pyplot import plot, show, close
 
-from raster_grid import RasterGrid, Point
+
+@dataclass
+class Point:
+    x: float
+    y: float
+
+    def distance_to(self, other: Point) -> float:
+        dx = other.x - self.x
+        dy = other.y - self.y
+        return sqrt(dx*dx + dy*dy)
 
 
-class RasterFunction:
-    def __init__(self,
-                 raster: RasterGrid,
-                 values: List[List[float]]) -> None:
-        self._raster = raster
-        self._values = values
+class PointCloud:
+    def __init__(self, points: List[Point]) -> None:
+        self._points = points
+
+    @property
+    def size(self) -> int:
+        return len(self._points)
+
+    # This is to make a `PointCloud` iterable, that is, to
+    # allow looping over its points like this:
+    #
+    # for p in point_cloud:
+    #     ...
+    def __iter__(self):
+        return iter(self._points)
+
+    def __getitem__(self, index: int) -> Point:
+        return self._points[index]
+
+    def get_nearest(self, p: Point) -> Point:
+        return min(self._points, key=lambda point: point.distance_to(p))
+
+    def get_nearest_point_index(self, p: Point) -> int:
+        return self._points.index(self.get_nearest(p))
+
+
+class DiscreteFunction:
+    def __init__(self, point_cloud: PointCloud, point_values: List[float]) -> None:
+        self._point_cloud = point_cloud
+        self._point_values = point_values
 
     def __call__(self, point: Point) -> float:
-        cell = self._raster.locate(point)
-        return self._values[cell.col_index][cell.row_index] if cell else nan
+        return self._point_values[
+            self._point_cloud.get_nearest_point_index(point)
+        ]
 
 
 class Line:
     def __init__(self, source: Point, target: Point) -> None:
-        self._points = (source, target)
+        self._source = source
+        self._target = target
+        self._vector = (
+            target.x - source.x,
+            target.y - source.y
+        )
 
     @property
     def source(self) -> Point:
-        return self._points[0]
+        return self._source
 
     @property
     def target(self) -> Point:
-        return self._points[1]
+        return self._target
+
+    def at(self, fraction: float) -> Point:
+        assert fraction >= 0.0 and fraction <= 1.0
+        return Point(
+            self._source.x + fraction*self._vector[0],
+            self._source.y + fraction*self._vector[1]
+        )
 
 
-def discretize(line: Line, number_of_samples: int) -> List[Point]:
-    step = (
-        (line.target.x - line.source.x)/(number_of_samples - 1),
-        (line.target.y - line.source.y)/(number_of_samples - 1)
-    )
-    return [
-        Point(
-            line.source.x + step[0]*float(i),
-            line.source.y + step[1]*float(i)
-        ) for i in range(number_of_samples)
-    ]
+def make_plot_data(function: DiscreteFunction,
+                   line: Line,
+                   number_of_samples: int = 1000) -> Tuple[List[float], List[float]]:
+    x = []
+    y = []
+    step_fraction = 1.0/(number_of_samples - 1)
+    for i in range(number_of_samples):
+        current = line.at(float(i)*step_fraction)
+        x.append(line.source.distance_to(current))
+        y.append(function(current))
+    return x, y
 
 
-def distance(p0: Point, p1: Point) -> float:
-    dx = p1.x - p0.x
-    dy = p1.y - p0.y
-    return sqrt(dx*dx + dy*dy)
-
-
-def plot_over_line(function: Callable[[Point], float],
+def plot_over_line(function: DiscreteFunction,
                    line: Line,
                    number_of_samples: int = 1000) -> None:
-    points = discretize(line, number_of_samples)
-    x = [distance(points[0], p) for p in points]
-    y = [function(p) for p in points]
+    x, y = make_plot_data(function, line, number_of_samples)
     plot(x, y)
     show()
     close()
 
 
-def _get_discrete_values(grid: RasterGrid) -> List[List[float]]:
-    def function(p: Point):
-        return sin(2.0*pi*p.x)*cos(2.0*pi*p.y)
-    discrete_values = [
-        [0.0 for _ in range(grid.resolution[0])]
-        for _ in range(grid.resolution[1])
-    ]
-    for cell in grid.cells:
-        discrete_values[cell.col_index][cell.row_index] = function(grid.center(cell))
-    return discrete_values
+def _test_function(position: Point) -> float:
+    return sin(2.0*pi*position.x)*cos(2.0*pi*position.y)
 
 
 if __name__ == "__main__":
-    grid = RasterGrid(
-        Point(0.0, 0.0),
-        size=(1.0, 1.0),
-        resolution=(100, 100)
+    print("Plotting along a line though a point cloud...")
+    print(
+        "Note that this may take some time since we are "
+        "doing a brute force nearest neighbor evaluation"
     )
-    values = _get_discrete_values(grid)
-    raster_function = RasterFunction(grid, values)
+
+    domain_size = (1.0, 1.0)
+    number_of_points = (50, 50)
+    dx = (
+        domain_size[0]/float(number_of_points[0]),
+        domain_size[1]/float(number_of_points[1])
+    )
+
+    point_cloud = PointCloud([
+        Point(float(i)*dx[0], float(j)*dx[1])
+        for i in range(number_of_points[0])
+        for j in range(number_of_points[1])
+    ])
+
+    point_values = [_test_function(p) for p in point_cloud]
+
     plot_over_line(
-        function=raster_function,
-        line=Line(
-            Point(0.0, 0.0),
-            Point(1.0, 1.0)
-        ),
+        DiscreteFunction(point_cloud, point_values),
+        Line(Point(0.0, 0.0), Point(1.0, 1.0)),
         number_of_samples=2000
     )
