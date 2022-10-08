5da64ca3ab42d80fcba708e14b30c0f7af56b345
Dennis Gläser <dennis.glaeser@iws.uni-stuttgart.de>
2022-10-08 10:30:27 +0200

    simplify raster grid exercise

--- raster_grid.py
+++ raster_grid.py
@@ -1,35 +1,52 @@
-from typing import Tuple, Iterable, Optional
-from dataclasses import dataclass
+# In this "solution", we have changed the names of the function
+# of `RasterGrid` to be more expressive. Moreover, we have computed
+# the discretization length of a single cell in the constructor in
+# order to not have to recompute that everytime in `cell_center`
+# (in the original exercise just called `c`).
+
+# We have also collected the arguments of the constructor into
+# data structures such that the total number of arguments is reduced
+# and can be type-checked, as each argument is of a different type.
+# That is, a point, a tuple of floats and a tuple of ints for the
+# origin, the size and the resolution. In the tests, we have created
+# helper functions in order to improve the readability.
+#
+# Another thing we did here was to not save the cells as one long list
+# in `RasterGrid`. Since the grid is fully structured, we can easily
+# construct the cells when they are requested, which saves us from
+# unneccesary memory overhead for very large rasters. Therefore, we
+# made `cells` a property that returns a generator expression -> something
+# that is iterable and each iterate is a cell. This does lazy evaluation,
+# which means that when the user calls `cells` on a raster, a lightweight
+# object is returned, which constructs a new cell upon iteration. This way
+# we don't have to store a large list of cells but construct them on-the-fly
+# when users iterate through them.
+
+from typing import Tuple, Iterable
 from math import isclose
+from dataclasses import dataclass
+
 
 @dataclass
 class Point:
     x: float
     y: float
 
-Resolution = Tuple[int, int]
 
 class RasterGrid:
     @dataclass
     class Cell:
-        col_index: int
-        row_index: int
+        _ix: int
+        _iy: int
 
     def __init__(self,
-                 lower_left: Point,
+                 origin: Point,
                  size: Tuple[float, float],
-                 resolution: Resolution) -> None:
-        self._lower_left = lower_left
-        self._size = size
+                 resolution: Tuple[int, int]) -> None:
+        self._origin = origin
         self._resolution = resolution
-        self._dx = (
-            size[0]/float(resolution[0]),
-            size[1]/float(resolution[1])
-        )
-
-    @property
-    def resolution(self) -> Resolution:
-        return self._resolution
+        self._dx = size[0]/float(resolution[0])
+        self._dy = size[1]/float(resolution[1])
 
     @property
     def number_of_cells(self) -> int:
@@ -43,94 +60,44 @@ class RasterGrid:
             for j in range(self._resolution[1])
         )
 
-    def center(self, cell: Cell) -> Point:
+    def cell_center(self, cell: Cell) -> Point:
         return Point(
-            self._lower_left.x + (float(cell.col_index) + 0.5)*self._dx[0],
-            self._lower_left.y + (float(cell.row_index) + 0.5)*self._dx[1]
+            self._origin.x + (float(cell._ix) + 0.5)*self._dx,
+            self._origin.y + (float(cell._iy) + 0.5)*self._dy
         )
 
-    def locate(self, point: Point) -> Optional[Cell]:
-        tolerance = 1e-6*max(self._dx[0], self._dx[1])
-        ix = self._get_x_index(point, tolerance)
-        iy = self._get_y_index(point, tolerance)
-        if ix < 0 or ix >= self._resolution[0]:
-            return None
-        if iy < 0 and iy >= self._resolution[1]:
-            return None
-        return self.Cell(ix, iy)
 
-    def _get_x_index(self, point: Point, tolerance: float) -> int:
-        x_min = self._lower_left.x
-        x_max = self._lower_left.x + self._size[0]
-        if abs(point.x - x_min) < tolerance:
-            return 0
-        elif abs(point.x - x_max) < tolerance:
-            return self._resolution[0] - 1
-        return int((point.x - x_min)/self._dx[0])
+def test_number_of_cells():
+    def _make_grid(resolution: Tuple[int, int]) -> RasterGrid:
+        return RasterGrid(Point(0., 0.), (1., 1.), resolution)
 
-    def _get_y_index(self, point: Point, tolerance: float) -> int:
-        y_min = self._lower_left.y
-        y_max = self._lower_left.y + self._size[1]
-        if abs(point.y - y_min) < tolerance:
-            return 0
-        elif abs(point.y - y_max) < tolerance:
-            return self._resolution[1] - 1
-        return int((point.y - y_min)/self._dx[1])
-
-
-def test_number_of_cells() -> None:
-    extent = (1.0, 1.0)
-    origin = Point(0.0, 0.0)
-    for nx, ny in zip([10, 10, 20, 20], [10, 20, 10, 20]):
-        grid = RasterGrid(origin, extent, (nx, ny))
-        assert grid.number_of_cells == nx*ny
-
-
-def test_locate_cell() -> None:
-    grid = RasterGrid(
-        lower_left=Point(0.0, 0.0),
-        size=(2.0, 2.0),
-        resolution=(2, 2)
-    )
-
-    for coords, expected_indices in [
-        ((0.0, 0.0), (0, 0)),
-        ((1.0, 1.0), (1, 1)),
-        ((0.5, 0.5), (0, 0)),
-        ((1.5, 0.5), (1, 0)),
-        ((0.5, 1.5), (0, 1)),
-        ((1.5, 1.5), (1, 1))
-    ]:
-        cell = grid.locate(Point(coords[0], coords[1]))
-        assert cell and cell.col_index == expected_indices[0] and cell.row_index == expected_indices[1]
-
-
-def test_cell_iterator() -> None:
-    grid = RasterGrid(
-        lower_left=Point(0.0, 0.0),
-        size=(2.0, 2.0),
-        resolution=(2, 2)
-    )
-    count = sum(1 for _ in grid.cells)
-    assert count == grid.number_of_cells
-
-    cell_indices_without_duplicates = set(list(
-        (cell.col_index, cell.row_index) for cell in grid.cells
-    ))
-    assert len(cell_indices_without_duplicates) == count
+    assert _make_grid((10, 10)).number_of_cells == 100
+    assert _make_grid((10, 20)).number_of_cells == 200
+    assert _make_grid((20, 10)).number_of_cells == 200
+    assert _make_grid((20, 20)).number_of_cells == 400
 
 
 def test_cell_center():
-    grid = RasterGrid(
-        lower_left=Point(0.0, 0.0),
-        size=(2.0, 2.0),
-        resolution=(2, 2)
-    )
-    for coords in [(0.5, 0.5), (1.5, 0.5), (0.5, 1.5), (1.5, 1.5)]:
-        point = Point(coords[0], coords[1])
-        cell = grid.locate(point)
-        assert cell and _is_equal(grid.center(cell), point)
+    grid = RasterGrid(Point(0.0, 0.0), (2.0, 2.0), (2, 2))
+    expected_centers = [
+        Point(0.5, 0.5),
+        Point(1.5, 0.5),
+        Point(0.5, 1.5),
+        Point(1.5, 1.5)
+    ]
+
+    def _is_equal(p0: Point, p1: Point) -> bool:
+        return isclose(p0.x, p1.x) and isclose(p0.y, p1.y)
+
+    for cell in grid.cells:
+        for center in expected_centers:
+            if _is_equal(center, grid.cell_center(cell)):
+                expected_centers.remove(center)
+
+    assert len(expected_centers) == 0
 
 
-def _is_equal(p0: Point, p1: Point) -> bool:
-    return isclose(p0.x, p1.x) and isclose(p0.y, p1.y)
+if __name__ == "__main__":
+    test_number_of_cells()
+    test_cell_center()
+    print("All tests passed")
